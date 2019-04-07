2105a0b8aa923265722731aaef2a40170139dd46
Linda Kovacs <lindaprovenza@yahoo.com>
2019-04-07 00:31:36 -0400

    Style compass view

--- views/CompassView.js
+++ views/CompassView.js
@@ -1,39 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import PropTypes from 'prop-types'
-import Navigator from '../utils/Navigator'
-import { Button } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  Image,
+  // ImageBackground,
+  Dimensions
+} from "react-native";
+import PropTypes from "prop-types";
+import Navigator from "../utils/Navigator";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import compass_bg from "./../assets/compass_bg.png";
+import compass_pointer from "./../assets/compass_pointer.png";
 
+const { height, width } = Dimensions.get("window");
 export default class CompassView extends React.Component {
-
-  constructor(props){
-    super(props)
-    this._navigator = new Navigator()
-    this.state = {heading: 0}
+  constructor(props) {
+    super(props);
+    this._navigator = new Navigator();
+    this.state = { heading: 0 };
   }
 
   updateHeading(heading) {
     this.setState(() => {
       return {
         heading: heading
-      }
-    })
+      };
+    });
   }
 
-  componentDidMount(){
-    this._subscription = this._navigator.watchHeading({}, this.updateHeading.bind(this))
+  componentDidMount() {
+    this._subscription = this._navigator.watchHeading(
+      {},
+      this.updateHeading.bind(this)
+    );
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this._subscription && this._subscription.remove();
     this._subscription = null;
   }
@@ -41,9 +45,77 @@ export default class CompassView extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Heading: {this.state.heading}</Text>
-        <Button title="Ok" onPress={() => this.props.setHeading(this.state.heading)}/>
+        <Image
+          source={compass_pointer}
+          style={{
+            height: height / 26,
+            resizeMode: "contain"
+          }}
+        />
+        {/* <ImageBackground
+          source={compass_bg}
+          style={{
+            height: width - 80,
+            justifyContent: "center",
+            alignItems: "center",
+            resizeMode: "contain",
+            transform: [{ rotate: 360 - this.state.heading + "deg" }]
+          }}
+        >
+          <Text style={styles.heading}>Heading: {this.state.heading}</Text>
+        </ImageBackground> */}
+
+        <Image
+          source={compass_bg}
+          style={{
+            height: width - 80,
+            justifyContent: "center",
+            alignItems: "center",
+            resizeMode: "contain",
+            transform: [{ rotate: 360 - this.state.heading + "deg" }]
+          }}
+        />
+        <Text style={styles.heading}>Heading: {this.state.heading}</Text>
+
+        {/* <Button title="Ok" onPress={() => this.props.setHeading(this.state.heading)}/> */}
+        <TouchableHighlight
+          onPress={() => this.props.setHeading(this.state.heading)}
+          underlayColor="white"
+          accessibilityLabel="Park Here button"
+        >
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>OK</Text>
+          </View>
+        </TouchableHighlight>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: "center",
+    backgroundColor: "white"
+  },
+  buttonText: {
+    padding: 20,
+    color: "#2196F3",
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+  heading: {
+    textAlign: "center",
+    color: "white",
+    fontSize: 30,
+    fontWeight: "bold",
+    padding: 20
+  }
+});
