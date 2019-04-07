3214ea568b15b8f7bc6c9fa6d598aa76b864523a
Linda Kovacs <lindaprovenza@yahoo.com>
2019-04-06 21:49:19 -0400

    Style home view

--- views/HomeView.js
+++ views/HomeView.js
@@ -1,50 +1,78 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import PropTypes from 'prop-types'
-import { Button } from 'react-native';
-import Navigator from '../utils/Navigator'
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import React from "react";
+import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
+import PropTypes from "prop-types";
+import Navigator from "../utils/Navigator";
 
 export default class HomeView extends React.Component {
-
-  constructor(props){
-    super(props)
-    this._navigator = new Navigator()
+  constructor(props) {
+    super(props);
+    this._navigator = new Navigator();
     this.state = {
       location: {},
-      errorMessage: ''
-    }
+      errorMessage: ""
+    };
   }
 
-  getLatLong(){
-    this._navigator.currentLocation({}).then(
-      (coords) => {
-        this.setState({location: coords})
-        this.props.setLocation(this.state.location)
-      }
-    ).catch(error => {
-      this.setState(() => {
-        return {
-          errorMessage: error.message
-        }
+  getLatLong() {
+    this._navigator
+      .currentLocation({})
+      .then(coords => {
+        this.setState({ location: coords });
+        this.props.setLocation(this.state.location);
       })
-    })
+      .catch(error => {
+        this.setState(() => {
+          return {
+            errorMessage: error.message
+          };
+        });
+      });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Button title="Park Here" onPress={this.getLatLong.bind(this)}/>
+        <Text style={styles.paragraph}>Parking Assistant</Text>
+        <TouchableHighlight
+          onPress={this.getLatLong.bind(this)}
+          underlayColor="white"
+          accessibilityLabel="Park Here button"
+        >
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Park Here</Text>
+          </View>
+        </TouchableHighlight>
         <Text>{this.state.errorMessage}</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 50,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#2196F3",
+    paddingBottom: 100
+  },
+  button: {
+    marginBottom: 30,
+    width: 260,
+    alignItems: "center",
+    backgroundColor: "#2196F3"
+  },
+  buttonText: {
+    padding: 20,
+    color: "white",
+    fontSize: 30,
+    fontWeight: "bold"
+  }
+});
