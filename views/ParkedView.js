7f2e11bc3f2eea3d56952bc8c5d624b0a5d5b3ce
Linda Kovacs <lindaprovenza@yahoo.com>
2019-04-07 01:52:05 -0400

    Style parked view

--- views/ParkedView.js
+++ views/ParkedView.js
@@ -1,32 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import PropTypes from 'prop-types'
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import CountDown from "react-native-countdown-component";
+import PropTypes from "prop-types";
 
 export default class ParkedView extends React.Component {
-
-  constructor(props){
-    super(props)
-    this.state = {timeRemaining: undefined}
+  constructor(props) {
+    super(props);
+    this.state = { timeRemaining: undefined };
   }
 
   updateTimeRemaining() {
     this.setState(() => {
       return {
         timeRemaining: this.props.parkingSpot.timeRemaining()
-      }
-    })
+      };
+    });
   }
   componentDidMount() {
-    this._countdownInterval = setInterval(this.updateTimeRemaining.bind(this), 1000);
+    this._countdownInterval = setInterval(
+      this.updateTimeRemaining.bind(this),
+      1000
+    );
   }
 
   componentWillUnmount() {
@@ -36,8 +30,30 @@ export default class ParkedView extends React.Component {
   render() {
     if (this.state.timeRemaining) {
       return (
+        // <View style={styles.container}>
+        //   <Text>You have {this.state.timeRemaining} minutes until you need to move your car</Text>
+        // </View>
+
         <View style={styles.container}>
-          <Text>You have {this.state.timeRemaining} minutes until you need to move your car</Text>
+          <Text style={styles.paragraph}>
+            Time remaining until you need to move your car
+          </Text>
+          <CountDown
+            // style={{
+            //   digitBgColor: "#2196F3",
+            //   digitTxtColor: "white",
+            //   timeTxtColor: "white"
+            // }}
+            until={this.state.timeRemaining}
+            //duration of countdown in seconds
+            timetoShow={["H", "M", "S"]}
+            //formate to show
+            onFinish={() => alert("finished")}
+            //on Finish call
+            onPress={() => alert("hello")}
+            //on Press call
+            size={35}
+          />
         </View>
       );
     } else {
@@ -47,6 +63,22 @@ export default class ParkedView extends React.Component {
         </View>
       );
     }
-
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#281de4",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 35,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#fff",
+    paddingBottom: 75
+  }
+});
